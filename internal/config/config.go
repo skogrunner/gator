7b442b1464a404c876a5eb9ae1a251e1e23e85df
Skogrunner <GaryKraght@comcast.net>
2026-05-22 21:02:50 -0700

    added SetUser and Read functions to config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,25 +1,56 @@
 package config
 
+import (
+	"os"
+	"encoding/json"
+)
+
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
-	db_url string
-	current_user_name string
+	Url string `json:"db_url"`
+	Name string `json:"current_user_name"`
 }
 
-func Read() Config {
-	return Config{}
+func Read() (Config, error) {
+	filepath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	content, err := os.ReadFile(filepath)
+	if err != nil {
+		return Config{}, err
+	}
+	var c Config
+	err = json.Unmarshal(content, &c)
+	if err != nil {
+		return Config{}, err
+	}
+	return c, nil
 }
 
-func (c Config) SetUser(name string) {
-	return
-}
-
-func getConfigFilePath() (string,error) {
-	return "", nil
+func (c Config) SetUser(name string) error {
+	c.Name = name
+	return write(c)
 }
 
 func write(cfg Config) error {
-	return nil
+	fileContent, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+	filepath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filepath, fileContent, 0666)
+}
+
+func getConfigFilePath() (string,error) {
+	hd, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return hd + "/.gatorconfig.json", nil
 }
 
--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"github.com/skogrunner/gator/internal/config"
 )
 
 func main() {
-	c := config.Read()
-	c.SetUser("gary")
-    fmt.Println(config.Read())
+	c, err := config.Read()
+	if err != nil {
+		fmt.Println("Error in Read", err)
+		os.Exit(1)
+	}
+	err = c.SetUser("gary")
+	if err != nil {
+		fmt.Println("Error in SetUser", err)
+		os.Exit(2)
+	}
+	c, err = config.Read()
+	if err != nil {
+		fmt.Println("Error in 2nd Read", err)
+		os.Exit(3)
+	}
+    fmt.Println(c)
 }
